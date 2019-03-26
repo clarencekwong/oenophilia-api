e324a2f4809dae6be8ccd8b024df4402ca8c3da9
github username <clarencekwong7@gmail.com>
2019-03-26 15:45:11 -0400

    fixed relationships hopefully

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,4 @@
 class Food < ApplicationRecord
+  has_many :foodwines
   has_many :wines, through: :foodwines
 end
--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,5 +1,7 @@
 class Wine < ApplicationRecord
-  has_many :reviews
+  has_many :foodwines
+  has_many :reviews, through: :foodwines
   has_many :foods, through: :foodwines
-  has_many :users, through: :reviews
+  has_many :favorite_wines
+  has_many :users, through: :favorite_wines
 end
